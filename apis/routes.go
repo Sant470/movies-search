36af3589b572dba6c153eb10fc7d76e54c506822
Sant Purushottam <purushotam.iitr@gmail.com>
2024-02-11 07:33:37 +0530

    fix: change error message format

--- apis/routes.go
+++ apis/routes.go
@@ -13,12 +13,12 @@ type Handler func(rw http.ResponseWriter, r *http.Request) error
 func (h Handler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	if err := h(rw, r); err != nil {
 		rw.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(rw, "Internal Server Error", err)
+		fmt.Fprintf(rw, "%s: error %s", "Internal Server Error", err.Error())
 	}
 }
 
 func InitSerachRoutes(r *chi.Mux, sh *v1.SearchHandler) {
-	r.Route("/v1/search", func(r chi.Router) {
+	r.Route("/api/v1/search", func(r chi.Router) {
 		r.Method(http.MethodGet, "/helloworld", Handler(sh.HelloWorld))
 	})
 }
